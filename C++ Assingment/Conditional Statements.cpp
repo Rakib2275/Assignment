f02205cc79ac535524cc88f08aedea0f6f794ca1
Rakibul Hasan <rakib932676@gmail.com>
2024-09-29 00:03:00 +0600

    Conditional Statements

---
+++ C++ Assingment/Conditional Statements.cpp
@@ -0,0 +1,21 @@
+//Conditional Statements
+//HackerRank
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number > 0) {
+        cout << "The number is positive." << endl;
+    } else {
+        cout << "The number is not positive." << endl;
+    }
+
+    return 0;
+}
+
